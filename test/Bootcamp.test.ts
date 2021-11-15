ad3b515670c27013cdc27d2b5cbd05eb96b041e7
chiranz <chiranpoudyal@gmail.com>
2021-11-15 07:23:36 +0530

    FIX minor issues

--- test/Bootcamp.test.ts
+++ test/Bootcamp.test.ts
@@ -1,28 +1,39 @@
 import { expect } from "chai";
+import "@nomiclabs/hardhat-ethers";
 import { ethers } from "hardhat";
-import {Contract} from "@ethersproject/contracts";
+import {
+  Bootcamp,
+  Bootcamp__factory,
+  Course,
+  Course__factory,
+} from "../typechain/index";
 import keccak256 from "keccak256";
-import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 
 describe("Bootcamp", async () => {
-  let course: Contract;
-  let bootcamp: Contract;
+  let course: Course;
+  let bootcamp: Bootcamp;
   let deployer: SignerWithAddress;
   let signer1: SignerWithAddress;
-  const CID = '0x9F86D081884C7D659A2FEAA0C55AD015A3BF4F1B2B0B822CD15D6C15B0F00A08';
+  const CID =
+    "0x9F86D081884C7D659A2FEAA0C55AD015A3BF4F1B2B0B822CD15D6C15B0F00A08";
 
   before(async (): Promise<any> => {
-
     [deployer, signer1] = await ethers.getSigners();
 
-    const Course = await ethers.getContractFactory("Course", deployer);
+    const Course = (await ethers.getContractFactory(
+      "Course",
+      deployer
+    )) as unknown as Course__factory;
     course = await Course.deploy();
     await course.deployed();
 
-    const Bootcamp = await ethers.getContractFactory("Bootcamp", signer1);
-    bootcamp = await Bootcamp.deploy(course.address, 'Bootcamp', 'US');
+    const Bootcamp = (await ethers.getContractFactory(
+      "Bootcamp",
+      signer1
+    )) as unknown as Bootcamp__factory;
+    bootcamp = await Bootcamp.deploy(course.address, "Bootcamp", "US");
     await bootcamp.deployed();
-
   });
   it("Should return correct 'Name' and 'location'", async () => {
     expect(await bootcamp.name()).to.equal("Bootcamp");
@@ -30,15 +41,20 @@ describe("Bootcamp", async () => {
   });
 
   it("Should return correct Course Implementation Address", async () => {
-    expect(await bootcamp.getImplementationAddress()).to.equal(course.address)
+    expect(await bootcamp.getImplementationAddress()).to.equal(course.address);
   });
 
   it("Should emit Cloned course address", async () => {
-    const clonedContractAddress = '0x' + keccak256(ethers.utils.RLP.encode([bootcamp.address, '0x01']))
-        .toString('hex').slice(-40);
-    const clonedContractAddressWithChecksum = ethers.utils.getAddress(clonedContractAddress);
-    await expect(bootcamp.createCourse(CID)).to.emit(bootcamp, "CourseCreated")
-        .withArgs(clonedContractAddressWithChecksum, CID);
+    const clonedContractAddress =
+      "0x" +
+      keccak256(ethers.utils.RLP.encode([bootcamp.address, "0x01"]))
+        .toString("hex")
+        .slice(-40);
+    const clonedContractAddressWithChecksum = ethers.utils.getAddress(
+      clonedContractAddress
+    );
+    await expect(bootcamp.createCourse(CID))
+      .to.emit(bootcamp, "CourseCreated")
+      .withArgs(clonedContractAddressWithChecksum, CID);
   });
-
 });
--- test/Course.test.ts
+++ test/Course.test.ts
@@ -1,43 +1,52 @@
- import { expect } from "chai";
- import { ethers } from "hardhat";
- import {Contract} from "@ethersproject/contracts";
- import {MerkleTree} from "merkletreejs";
- import keccak256 from "keccak256";
+import { expect } from "chai";
+import "@nomiclabs/hardhat-ethers";
+import { ethers } from "hardhat";
+import { Course__factory, Course } from "../typechain/index";
+import { MerkleTree } from "merkletreejs";
+import keccak256 from "keccak256";
 
- describe("Course", async () => {
-     let course: Contract;
-     let students;
-     let merkleTree;
-     let root: string;
-     let leaf: string;
-     let proof: string[];
-     const CID = '0x9F86D081884C7D659A2FEAA0C55AD015A3BF4F1B2B0B822CD15D6C15B0F00A08';
+describe("Course", async () => {
+  let course: Course;
+  let students;
+  let merkleTree;
+  let root: string;
+  let leaf: string;
+  let proof: string[];
+  const CID =
+    "0x9F86D081884C7D659A2FEAA0C55AD015A3BF4F1B2B0B822CD15D6C15B0F00A08";
 
-     before(async () => {
-         const [deployer] = await ethers.getSigners();
+  before(async () => {
+    const [deployer] = await ethers.getSigners();
 
-         const Course = await ethers.getContractFactory("Course", deployer);
-         course = await Course.deploy();
-         await course.initialize(CID, deployer.getAddress());
+    const Course = (await ethers.getContractFactory(
+      "Course",
+      deployer
+    )) as unknown as Course__factory;
+    course = await Course.deploy();
 
-         students = await ethers.provider.listAccounts();
-         merkleTree = new MerkleTree(students, keccak256, { hashLeaves: true, sortPairs: true });
-         root = merkleTree.getHexRoot();
-         leaf = keccak256(students[4]);
-         proof = merkleTree.getHexProof(leaf);
-     });
+    await course.initialize(CID, deployer.address);
 
-     it('Returns Course CID', async () => {
-         expect(await course.getCourseCID()).to.equal(CID);
-     })
+    students = await ethers.provider.listAccounts();
+    merkleTree = new MerkleTree(students, keccak256, {
+      hashLeaves: true,
+      sortPairs: true,
+    });
+    root = merkleTree.getHexRoot();
+    leaf = keccak256(students[4]);
+    proof = merkleTree.getHexProof(leaf);
+  });
 
-     it("Should emit graduate event", async () => {
-         await expect(course.graduate(root, CID)).to.emit(course, "Graduate")
-             .withArgs(root, CID);
-     })
+  it("Returns Course CID", async () => {
+    expect(await course.getCourseCID()).to.equal(CID);
+  });
 
-     it('returns true for a valid Merkle proof', async () => {
-         expect(await course.isCertified(proof, leaf, root)).to.equal(true);
-     });
+  it("Should emit graduate event", async () => {
+    await expect(course.graduate(root, CID))
+      .to.emit(course, "Graduate")
+      .withArgs(root, CID);
+  });
 
- })
+  it("returns true for a valid Merkle proof", async () => {
+    expect(await course.isCertified(proof, leaf, root)).to.equal(true);
+  });
+});
