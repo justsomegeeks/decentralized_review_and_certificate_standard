154bc689b211c2c3e63b7a789a71d287de8fb7a5
echolinq <echolinq@protonmail.com>
2021-11-15 01:51:13 -0500

    test updates

---
+++ test/Review.test.ts
@@ -0,0 +1,92 @@
+ import { expect } from "chai";
+ import {artifacts, ethers } from "hardhat";
+ import {Contract} from "@ethersproject/contracts";
+ import { MerkleTree } from "merkletreejs";
+ import keccak256 from "keccak256";
+ import {
+     Bootcamp,
+     Bootcamp__factory,
+     Course,
+     Course__factory,
+     Review,
+     Review__factory
+ } from "../typechain/index";
+ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+
+ describe("Review", async () => {
+     let course: Course;
+     let bootcamp: Bootcamp;
+     let review: Review;
+     let bootcampCourse: Course;
+     let students: string[];
+     let merkleTree: MerkleTree;
+     let root: string;
+     let leaf: string;
+     let proof: string[];
+     let deployer: SignerWithAddress;
+     let signer1: SignerWithAddress;
+     const CID = '0x9F86D081884C7D659A2FEAA0C55AD015A3BF4F1B2B0B822CD15D6C15B0F00A08';
+
+     before(async () => {
+         [deployer, signer1] = await ethers.getSigners();
+
+         const Course = (await ethers.getContractFactory(
+             "Course",
+             deployer
+         )) as unknown as Course__factory;
+         course = await Course.deploy();
+         await course.deployed();
+
+         const Review = (await ethers.getContractFactory(
+             "Review",
+             deployer
+         )) as unknown as Review__factory;
+         review = await Review.deploy();
+         await review.deployed();
+
+         const Bootcamp = (await ethers.getContractFactory(
+             "Bootcamp",
+             signer1
+         )) as unknown as Bootcamp__factory;
+         bootcamp = await Bootcamp.deploy(course.address, "Bootcamp", "US");
+         await bootcamp.deployed();
+
+         students = await ethers.provider.listAccounts();
+         merkleTree = new MerkleTree(students, keccak256, {
+             hashLeaves: true,
+             sortPairs: true,
+         });
+         root = merkleTree.getHexRoot();
+         leaf = keccak256(students[1]);
+         proof = merkleTree.getHexProof(leaf);
+     });
+
+     it('Should emit Review Event', async () => {
+         const clonedContractAddress =
+             "0x" +
+             keccak256(ethers.utils.RLP.encode([bootcamp.address, "0x01"]))
+                 .toString("hex")
+                 .slice(-40);
+         const clonedContractAddressWithChecksum = ethers.utils.getAddress(
+             clonedContractAddress
+         );
+
+         await bootcamp.createCourse(CID);
+         const courseAddress = clonedContractAddressWithChecksum;
+         const BootcampCourse = (await ethers.getContractAt(
+             "Course",
+             courseAddress,
+             signer1
+         )) as unknown as Course__factory
+
+         bootcampCourse = await BootcampCourse.attach(courseAddress);
+
+         await expect(bootcampCourse.graduate(root, CID))
+             .to.emit(bootcampCourse, "Graduate")
+             .withArgs(root, CID);
+
+         await expect(review.connect(signer1).reviewCourse(courseAddress, CID, proof, root)).to
+             .emit(review, "NewReview").withArgs(await signer1.getAddress(), CID)
+     })
+
+ })
