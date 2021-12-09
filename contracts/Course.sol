279622f50069acb0e5700dc44310725e750af4f8
Echolinq <echolinq@protonmail.com>
2021-12-09 12:01:33 -0500

    updated formatting updates

--- contracts/Course.sol
+++ contracts/Course.sol
@@ -9,12 +9,9 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 
 contract Course is Initializable, OwnableUpgradeable {
-    // EVENTS
-    event Graduate(bytes32 merkelProof, string courseCID);
 
-    // STATE
+    event Graduate(bytes32 merkelProof, string courseCID);
     string private courseCID;
-    //      merkle proof => cid
     mapping(bytes32 => string) public graduations;
 
     function initialize(string memory _courseCID, address _owner)
@@ -26,6 +23,11 @@ contract Course is Initializable, OwnableUpgradeable {
         transferOwnership(_owner);
     }
 
+    function graduate(bytes32 _hash, string memory _cid) external onlyOwner {
+        graduations[_hash] = _cid;
+        emit Graduate(_hash, _cid);
+    }
+
     function isCertified(
         bytes32[] memory proof,
         bytes32 leaf,
@@ -33,13 +35,6 @@ contract Course is Initializable, OwnableUpgradeable {
     ) external view returns (bool) {
         require(bytes(graduations[root]).length > 0, "Root not found");
         return MerkleProofUpgradeable.verify(proof, root, leaf);
-    }
-
-    // TODO: only owner should be allowed to call this function.
-    // Should owner be the Bootcamp contract or EOA?
-    function graduate(bytes32 _hash, string memory _cid) external onlyOwner {
-        graduations[_hash] = _cid;
-        emit Graduate(_hash, _cid);
     }
 
     function getCourseCID() external view returns (string memory) {
